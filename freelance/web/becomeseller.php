ff0f31679a3effd42c2b19683ec020bf3b3a777d
JeetDhake <147963729+JeetDhake@users.noreply.github.com>
2023-10-20 08:56:48 +0530

    Add files via upload

--- freelance/web/becomeseller.php
+++ freelance/web/becomeseller.php
@@ -117,29 +117,6 @@ if (isset($_POST['submit'])) {
 
     </div>
 </body>
-<script>
-    const drop = document.getElementById("drop");
-    const inputf = document.getElementById("image");
-    const imgview = document.getElementById("img-view");
-
-    inputf.addEventListener("change", uploadImage);
-
-    function uploadImage() {
-        inputf.files[0];
-        let imgLink = URL.createObjectURL(inputf.files[0]);
-        imgview.style.backgroundImage = `url(${imgLink})`;
-        imgview.textContent = "";
-
-    }
-
-    drop.addEventListener("dragover", function(e) {
-        e.preventDefault();
-    });
-    drop.addEventListener("drop", function(e) {
-        e.preventDefault();
-        inputf.files = e.dataTransfer.files;
-        uploadImage();
-    });
-</script>
+<script src="becomeseller.js"></script>
 
 </html>
